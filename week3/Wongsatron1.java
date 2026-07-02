55c8f7b47bf363bf5a189782760547af51198402
694259009-creator <694259009@webmail.npru.ac.th>
2026-07-02 16:16:55 +0700

    add Wongsatron1.java

---
+++ week3/Wongsatron1.java
@@ -0,0 +1,9 @@
+void main() {
+    IO.println("Enter Int");
+    int num1 = Integer.parseInt(IO.readln());
+    IO.println("Enter Int");
+    int num2 = Integer.parseInt(IO.readln());
+    int total = num1 + num2;
+    
+    IO.println(" คำตอบ " + total);
+}
