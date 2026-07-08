0969b660478511e5bb51a9938e468f92e8b47764
694259009-creator <694259009@webmail.npru.ac.th>
2026-07-09 05:25:55 +0700

    Week5

---
+++ week5/Wongsatron6.java
@@ -0,0 +1,22 @@
+void main() {
+    IO.print(" Enter the base of the triangle ");
+    double base = Double.parseDouble(IO.readln());
+
+    IO.print(" Enter the height of the triangle ");
+    double high = Double.parseDouble(IO.readln());
+
+    double triangleArea = (1.0/2.0)*base *high;
+
+    IO.println(" Area of the triangle %.2f ".formatted(triangleArea) + " square units ");
+
+
+    IO.print(" Enter the length of the rectangle ");
+    double side = Double.parseDouble(IO.readln());
+
+    IO.print(" Enter the width of the rectangle ");
+    double side1 = Double.parseDouble(IO.readln());
+
+    double Squarearea = side *side1;
+
+    IO.println(" Area of the rectangle %.2f ".formatted(Squarearea) + " square units ");
+}
